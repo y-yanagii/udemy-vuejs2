49e243c23e4f1071f91c38a7542b6f1b72d5a5f1
y-yanagii <yyanagisawa555@gmail.com>
2021-03-24 00:36:50 +0900

    chapter56

--- udemy-vuejs/src/main.js
+++ udemy-vuejs/src/main.js
@@ -1,8 +1,13 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+// コンソールにビルドした時のおきまりを説明してくれる文言を流す(trueの場合)
 Vue.config.productionTip = false
+
+// vueファイルはimportして使う。importするとコンポーネントのオブジェクトを返す
+console.log(App);
 
 new Vue({
   render: h => h(App),
 }).$mount('#app')
+// .$mount('#app')はel: '#app'と同様
