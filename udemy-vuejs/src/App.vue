f66549993a326c90b9c4382526cbc9e70e33e115
y-yanagii <yyanagisawa555@gmail.com>
2021-03-29 18:45:41 +0900

    08

--- udemy-vuejs/src/App.vue
+++ udemy-vuejs/src/App.vue
@@ -1,24 +1,34 @@
 <template>
   <div>
-    <LikeHeader></LikeHeader>
-    <h2>{{ number }}</h2>
+    <LikeHeader>
+      <h3>はじめまして</h3>
+    </LikeHeader>
     <LikeNumber :total-number="number" test-props="test" @my-click="incrementNumber"></LikeNumber>
-    <LikeNumber :total-number="number" test-props="test"></LikeNumber>
+    <button @click="currentComponent = 'Home'">Home</button>
+    <button @click="currentComponent = 'About'">About</button>
+    <keep-alive>
+      <component :is="currentComponent"></component>
+    </keep-alive>
   </div>
 </template>
 
 <script>
 import LikeHeader from "./components/LikeHeader.vue";
+import About from "./components/About.vue";
+import Home from "./components/Home.vue";
 
 export default {
   // ローカルコンポーネント登録
   data() {
     return {
-      number: 14
+      number: 14,
+      currentComponent: "Home"
     }
   },
   components: {
-    LikeHeader
+    LikeHeader,
+    About,
+    Home
   },
   methods: {
     incrementNumber(value) {
@@ -33,4 +43,5 @@ export default {
 div {
   border: 1px solid blue;
 }
+
 </style>
